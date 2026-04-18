6d01bb57829cea582b6e2cb45164182b7429c458
gberuashvili15G <g.beruashvili@noxtton.com>
2026-04-18 18:11:53 +0400

    Update ADMIN_DEV_TOKEN to new value for security compliance

--- admin-panel/src/lib/dev-token.ts
+++ admin-panel/src/lib/dev-token.ts
@@ -8,4 +8,4 @@
  * Put the "token" value here and push. Refresh when expired (7 days).
  */
 export const ADMIN_DEV_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2OWFmZDk1NjRkYzhmZjhjZWUxMTI3NmIiLCJpYXQiOjE3NzU1OTYxNDMsImV4cCI6MTc3NjIwMDk0M30.4qYN3s7IU-pE4g6e7HXye3OHBSfrotuz-XSEWWLjxY0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2OWFmZDk1NjRkYzhmZjhjZWUxMTI3NmIiLCJpYXQiOjE3NzY1MjE0ODIsImV4cCI6MTc3NzEyNjI4Mn0.5V6DWuO17Yhmy5QS-AULORd8N_YIeq5YaZEfOZsTJlE";
